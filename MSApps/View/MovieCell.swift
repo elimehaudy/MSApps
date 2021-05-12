bd38d996a46efe38785f84b98a499df82eff0460
Eli Mehaudy <elimehaudy@Elis-MacBook-Pro.local>
2021-05-12 18:43:42 +0300

    Custom MovieCell added

---
+++ MSApps/View/MovieCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieCell.swift
+//  MSApps
+//
+//  Created by Eli Mehaudy on 12/05/2021.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
